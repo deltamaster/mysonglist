6ee522798ddd46c6b15ac2b6a3e4c631fcb78e3d
Hansen Hu <hansen.hu@gsgh.cn>
2024-05-26 11:49:33 +0800

    update list

--- src/SongList.tsx
+++ src/SongList.tsx
@@ -1,65 +1,54 @@
 import React, { useEffect, useRef } from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import { mockSongs } from "./mockSongs";
 import SongItem from "./SongItem";
+
+const scrollAnimation = keyframes`
+  from {
+    transform: translateY(0);
+  }
+  to {
+    transform: translateY(-50%);
+  }
+`;
+
+const GridContainer = styled.div`
+  overflow-y: hidden; /* Enable vertical scrolling */
+  max-height: 90vh; /* Adjust based on your screen size */
+
+  &::before,
+  &::after {
+    content: "";
+    height: 50%; /* Same as the translateY value in the animation */
+  }
+
+  & > div:first-of-type {
+    color: black;
+    animation: ${scrollAnimation} 100s linear infinite; /* Adjust timing as needed */
+  }
+`;
 
 const Grid = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(30px, max-content));
   gap: 10px;
-  overflow-y: hidden; /* Enable vertical scrolling */
-  max-height: 90vh; /* Adjust based on your screen size */
 `;
 
 const SongList: React.FC = () => {
   const gridRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    const scrollContainer = gridRef.current;
-
-    // Function to start the automatic scrolling
-    const startScrolling = () => {
-      if (scrollContainer) {
-        // Set the speed of scrolling (smaller numbers = faster scrolling)
-        const speed = 2;
-        let topPosition = scrollContainer.scrollTop;
-
-        // Scroll function
-        const scroll = () => {
-          topPosition += speed;
-          scrollContainer.scrollTop = topPosition;
-
-          // Reset scroll position to top when it reaches the bottom
-          if (
-            topPosition >=
-            scrollContainer.scrollHeight - scrollContainer.clientHeight
-          ) {
-            topPosition = 0;
-          }
-        };
-
-        // Start the interval for scrolling
-        const intervalId = setInterval(scroll, 100);
-
-        // Clear the interval when component unmounts
-        return () => clearInterval(intervalId);
-      }
-    };
-
-    // Start scrolling
-    startScrolling();
-  }, []);
-
   return (
-    <Grid ref={gridRef}>
-      {mockSongs.map((song) => (
-        <SongItem key={song.id} song={song} />
-      ))}
-      <div style={{ gridColumnStart: "-1" }} />
-      {mockSongs.map((song) => (
-        <SongItem key={song.id} song={song} />
-      ))}
-    </Grid>
+    <GridContainer>
+      <Grid ref={gridRef}>
+        {mockSongs.map((song) => (
+          <SongItem key={song.id} song={song} />
+        ))}
+        <div style={{ gridColumnStart: "-1" }} />
+        {mockSongs.map((song) => (
+          <SongItem key={song.id} song={song} />
+        ))}
+      </Grid>
+    </GridContainer>
   );
 };
 
